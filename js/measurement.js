930b7e037c803f782cce525b9bd6d301f8f2ecb4
Michal Dydo <michal1.dydo@gmail.com>
2016-11-22 23:03:57 +0100

    New upload

--- js/measurement.js
+++ js/measurement.js
@@ -6,7 +6,6 @@ function getData() {
 
 function showMeasurement(data) {
 	var dataAddress = data.Data.DeviceTelemetryModels;
-
 	dataAddress.map(function (item) {
 		addBox(item.Timestamp, item.Telemetries.cputemp, item.Telemetries.odleglosc, item.Telemetries.temp, item.Telemetries.wilgotDHT);
 	});
@@ -16,9 +15,13 @@ function addBox(time, data1, data2, data3, data4) {
 	var model = $('<div class="box-other"></div>');
 	var paragraph1 = $('<p class="textHead">' + time.substr(0, 10) + " " + time.substr(11, 8) + '</p>');
 	var paragraph2 = $('<p class="text">' + data1.Description + ': ' + data1.Value + data1.Unit + '</p>');
+	if (data1.Value > 36) { paragraph2.css("background-color", "orange");}
+	if (data1.Value > 40) { paragraph2.css("background-color", "red");}
 	var paragraph3 = $('<p class="text">' + data2.Description + ': ' + data2.Value + data2.Unit + '</p>');
 	var paragraph4 = $('<p class="text">' + data3.Description + ': ' + data3.Value + data3.Unit + '</p>');
 	var paragraph5 = $('<p class="text">' + data4.Description + ': ' + data4.Value + data4.Unit + '</p>');
+	if (data4.Value > 35) { paragraph5.css("background-color", "orange");}
+	if (data4.Value > 40) { paragraph5.css("background-color", "red");}
 
 	model.appendTo('.mainSection');
 	paragraph1.appendTo(model);
--- js/parameters.js
+++ js/parameters.js
@@ -1,40 +1,52 @@
 var devicesList = "https://molosapi.azurewebsites.net/api/v1/telemetry/FENEK01/last";
 
 function getData() {
-  $.getJSON(devicesList, showDevices);
+    $.getJSON(devicesList, showDevices);
 }
 
 function showDevices(data) {
-  var dataAddress = data.Data.DeviceTelemetryModels[0].Telemetries;
-  var deviceParameters = ['cputemp', 'odleglosc', 'temp', 'tempDHT', 'wilgotDHT'];
-  deviceParameters.forEach(function (item) {
-    addBox(dataAddress[item]);
-  })
+    var dataAddress = data.Data.DeviceTelemetryModels[0].Telemetries;
+    var deviceParameters = ['cputemp', 'odleglosc', 'temp', 'tempDHT', 'wilgotDHT'];
+    deviceParameters.forEach(function(item) {
+        addBox(dataAddress[item]);
+    })
+    if (dataAddress.cputemp.Value > 36) {
+        $('.box:nth-child(1)').css("background-color", "orange");}
+    if (dataAddress.cputemp.Value > 40) {
+        $('.box:nth-child(1)').css("background-color", "red");}
+    if (dataAddress.temp.Value > 25) {
+        $('.box:nth-child(3)').css("background-color", "orange");}
+    if (dataAddress.temp.Value > 30) {
+        $('.box:nth-child(3)').css("background-color", "red");}
+    if (dataAddress.wilgotDHT.Value > 35) {
+        $('.box:nth-child(5)').css("background-color", "orange");}
+    if (dataAddress.wilgotDHT.Value > 40) {
+        $('.box:nth-child(5)').css("background-color", "red");}
 }
 
 function addBox(device) {
-  var model = $('<div class="box"></div>');
-  model.appendTo('.mainSection');
+    var model = $('<div class="box"></div>');
+    model.appendTo('.mainSection');
 
-  var paragraph1 = $('<p class="text1">' + device.Description + ':' + '</p>');
-  var paragraph2 = $('<p class="text2">' + device.Value + device.Unit + '</p>');
+    var paragraph1 = $('<p class="text1">' + device.Description + ':' + '</p>');
+    var paragraph2 = $('<p class="text2">' + device.Value + device.Unit + '</p>');
 
-  model.appendTo('.mainSection');
-  paragraph1.appendTo(model);
-  paragraph2.appendTo(model);
+    model.appendTo('.mainSection');
+    paragraph1.appendTo(model);
+    paragraph2.appendTo(model);
 }
 
-$(document).ready(function () {
-  getData();
+$(document).ready(function() {
+    getData();
 
-  $('#sortable').sortable({
-    change: function (event, ui) {
-      ui.placeholder.css({ visibility: 'visible', boxShadow: 'none', border: '1px dotted grey', background: 'rgba(255, 255, 255, 0.5)' });
-    },
-    tolerance: 'touch',
-    drop: function () {
-      alert('delete!');
-    }
-  });
-  $('#item').sortable();
+    $('#sortable').sortable({
+        change: function(event, ui) {
+            ui.placeholder.css({ visibility: 'visible', boxShadow: 'none', border: '1px dotted grey', background: 'rgba(255, 255, 255, 0.5)' });
+        },
+        tolerance: 'touch',
+        drop: function() {
+            alert('delete!');
+        }
+    });
+    $('#item').sortable();
 });
