f6d955aac90352911b3348368b43cb5b61409eea
Michal Dydo <michal1.dydo@gmail.com>
2016-11-22 20:22:07 +0100

    New upload

--- js/list.js
+++ js/list.js
@@ -1,24 +1,24 @@
 var equipmentList = "https://molosapi.azurewebsites.net/api/v1/devices";
 
 function getData() {
-  $.getJSON(equipmentList, showEequipment);
+	$.getJSON(equipmentList, showEequipment);
 }
 
 function showEequipment(data) {
-  var dataAddress = data.Data;
-  dataAddress.map(function (item) {
-    var device = item.DeviceProperties;
-    addBox(device)
-  });
+	var dataAddress = data.Data;
+	dataAddress.map(function (item) {
+		var device = item.DeviceProperties;
+		addBox(device)
+	});
 }
 
 function addBox(equipment) {
 
-  var model = $('<div class="box-models"></div>');
-  model.appendTo('.mainSection');
-  for (var variable in equipment) {
-    model.append('<p>' + variable + ': ' + equipment[variable] + '<p>');
-  }
+	var model = $('<div class="box-models"></div>');
+	model.appendTo('.mainSection');
+	for (var variable in equipment) {
+		model.append('<p>' + variable + ': ' + equipment[variable] + '<p>');
+	}
 }
 
 $(document).ready(function () {
--- js/measurement.js
+++ js/measurement.js
@@ -1,31 +1,31 @@
 var measurementLast2Min = "https://molosapi.azurewebsites.net/api/v1/telemetry/FENEK01/2m";
 
 function getData() {
-  $.getJSON(measurementLast2Min, showMeasurement);
+	$.getJSON(measurementLast2Min, showMeasurement);
 }
 
 function showMeasurement(data) {
-  var dataAddress = data.Data.DeviceTelemetryModels;
+	var dataAddress = data.Data.DeviceTelemetryModels;
 
-  dataAddress.map(function (item) {
-    addBox(item.Timestamp, item.Telemetries.cputemp, item.Telemetries.odleglosc, item.Telemetries.temp, item.Telemetries.wilgotDHT);
-  });
+	dataAddress.map(function (item) {
+		addBox(item.Timestamp, item.Telemetries.cputemp, item.Telemetries.odleglosc, item.Telemetries.temp, item.Telemetries.wilgotDHT);
+	});
 }
 
 function addBox(time, data1, data2, data3, data4) {
-  var model = $('<div class="box-other"></div>');
-  var paragraph1 = $('<p class="textHead">' + time.substr(0, 10) + " " + time.substr(11, 8) + '</p>');
-  var paragraph2 = $('<p class="text">' + data1.Description + ': ' + data1.Value + data1.Unit + '</p>');
-  var paragraph3 = $('<p class="text">' + data2.Description + ': ' + data2.Value + data2.Unit + '</p>');
-  var paragraph4 = $('<p class="text">' + data3.Description + ': ' + data3.Value + data3.Unit + '</p>');
-  var paragraph5 = $('<p class="text">' + data4.Description + ': ' + data4.Value + data4.Unit + '</p>');
+	var model = $('<div class="box-other"></div>');
+	var paragraph1 = $('<p class="textHead">' + time.substr(0, 10) + " " + time.substr(11, 8) + '</p>');
+	var paragraph2 = $('<p class="text">' + data1.Description + ': ' + data1.Value + data1.Unit + '</p>');
+	var paragraph3 = $('<p class="text">' + data2.Description + ': ' + data2.Value + data2.Unit + '</p>');
+	var paragraph4 = $('<p class="text">' + data3.Description + ': ' + data3.Value + data3.Unit + '</p>');
+	var paragraph5 = $('<p class="text">' + data4.Description + ': ' + data4.Value + data4.Unit + '</p>');
 
-  model.appendTo('.mainSection');
-  paragraph1.appendTo(model);
-  paragraph2.appendTo(model);
-  paragraph3.appendTo(model);
-  paragraph4.appendTo(model);
-  paragraph5.appendTo(model);
+	model.appendTo('.mainSection');
+	paragraph1.appendTo(model);
+	paragraph2.appendTo(model);
+	paragraph3.appendTo(model);
+	paragraph4.appendTo(model);
+	paragraph5.appendTo(model);
 }
 
 $(document).ready(function () {
